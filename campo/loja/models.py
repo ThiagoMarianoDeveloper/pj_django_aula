6ec2cf9a0b9d69ae85f88c4b50e51a286955ff63
Thiago Mariano <134088653+ThiagoMarianoDeveloper@users.noreply.github.com>
2024-03-21 18:09:32 +0000

    edição no "models.py"

--- campo/loja/models.py
+++ campo/loja/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Produto(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField()
-    created_date = models.DateTimeFields(default = timezone.now)
+    created_date = models.DateTimeField(default = timezone.now)
 
         def __str__(self):
             return self.nome
