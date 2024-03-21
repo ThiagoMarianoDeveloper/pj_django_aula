e46327371e133739d348a3d34dfacd4ce96055b9
Thiago Mariano <134088653+ThiagoMarianoDeveloper@users.noreply.github.com>
2024-03-21 18:06:28 +0000

    Criação da "loja"

---
+++ campo/loja/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone 
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    created_date = models.DateTimeFields(default = timezone.now)
+
+        def __str__(self):
+            return self.nome
