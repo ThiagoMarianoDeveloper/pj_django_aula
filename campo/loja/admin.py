6c395d65ef379263ac496fb50d20ed65cac4759d
Thiago Mariano <134088653+ThiagoMarianoDeveloper@users.noreply.github.com>
2024-03-22 18:23:35 +0000

    atualizando o "models.py"

--- campo/loja/admin.py
+++ campo/loja/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, Dados_usuario, Fornecedor
 
 admin.site.register(Produto)
+admin.site.register(Dados_usuario)
+admin.site.register(Fornecedor)
--- campo/loja/models.py
+++ campo/loja/models.py
@@ -1,10 +1,33 @@
 from django.db import models
-from django.utils import timezone 
+from django.utils import timezone
 
-class Produto(models.Model):
+
+class Produto (models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField()
-    created_date = models.DateTimeField(default = timezone.now)
+    valor = models.IntegerField()
+    valores = models.IntegerField()
+    validade = models.DateTimeField(default = timezone.now)
+    estoque = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+
+class Dados_usuario (models.Model):
+    rua = models.CharField(max_length=200)
+    numero = models.IntegerField()
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nome
+
+
+
+class Fornecedor (models.Model):
+    nome = models.CharField(max_length=200)
+    cnpj = models.IntegerField()
 
     def __str__(self):
         return self.nome
