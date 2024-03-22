bc9c52840ee86f2c7e5db8f84f2cb42c7c6227ff
Thiago Mariano <134088653+ThiagoMarianoDeveloper@users.noreply.github.com>
2024-03-22 19:36:06 +0000

    Os "urls"

--- campo/loja/views.py
+++ campo/loja/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def pesquisar(request):
+    return render(request, 'loja/pesquisar.html',{})
+
+#def cadastrar(request):
+ #   print("chegou no cadastrar")
